ac41c0b9e9041a36e9d845b577421e760260a3eb
shadeemerhi <shadmerhi@gmail.com>
2023-02-22 19:30:26 -0700

    add template seed script

---
+++ prisma/seed.ts
@@ -0,0 +1,13 @@
+import prisma from "./client";
+
+async function main() {}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
